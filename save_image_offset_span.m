91deddfa94d0c5cd686ab39109860dc2b8ed843f
thinkiv\iuliuv <iuliuv@gmail.com>
2016-09-08 21:48:59 +0300

    wrap arround

--- save_image_offset_span.m
+++ save_image_offset_span.m
@@ -1,17 +1,15 @@
 %offsets and spans - 16 bit integers...
 
 function save_image_offset_span(phase, offsets, spans, file_name_prefix)
-  
+    
   pkg load mapping;
   phase = wrapTo2Pi(phase);
   for offset = offsets
     for span = spans
-      if span+offset<2^16
-        phase_im = uint16(offset + span*phase/2/pi);
+        phase_im = uint16(mod(offset + span*phase/2/pi, 2^16));
         imwrite(phase_im, 
           [file_name_prefix '_offs' sprintf('%05i', offset) '_spn' sprintf('%05i', span) '.tif'],
           'tiff');
-      end
     end
   end  
   
