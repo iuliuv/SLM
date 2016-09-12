4a6a57f80b5a61e44b3b2f6131edbe3cfda33320
thinkiv\iuliuv <iuliuv@gmail.com>
2016-09-12 20:30:24 +0300

    SaveCheckerBoardOffsetSpan.m

--- SaveCheckerBoardOffsetSpan.m
+++ SaveCheckerBoardOffsetSpan.m
@@ -1,3 +1,3 @@
-function generate_checker_board(ImSize, sqsize, offsets, spans, file_name_prefix);
+function SaveCheckerBoardOffsetSpan(ImSize, sqsize, offsets, spans, file_name_prefix);
   save_image_offset_span(CheckerBoard(ImSize, SqSize)*2*pi, offsets, spans, file_name_prefix);  
 endfunction
